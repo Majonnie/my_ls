bbe4e6d2a7cf597d9fc6d35d9a1f13417896c150
Majonnie <bonhommejulie2003@gmail.com>
2022-01-04 09:19:40 +0100

    [TEST] Récupération nom dossier + test si ouverture/fermeture dossier possible

---
+++ tests/test_dir_copy.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <dirent.h> /*Permet d'utiliser opendir, readdir, etc*/
+
+int main(int argc, char **argv)
+{
+    printf("%d\n", argc);
+    printf("%s\n", argv[1]);
+
+    DIR *rep = NULL;
+    rep = opendir(argv[1]); /* Ouverture d'un dossier */
+
+    if (rep == NULL) /* Si le dossier n'a pas pu être ouvert */
+    {
+        puts("Problème d'ouverture.");
+        exit(1); /* (mauvais chemin par exemple) */
+    }
+
+    puts("Le dossier a ete ouvert avec succes");
+
+    if (closedir(rep) == -1) /* S'il y a eu un souci avec la fermeture */
+    {
+        puts("Problème de fermeture.");
+        exit(-1);
+    }
+
+    puts("Le dossier a ete ferme avec succes");
+
+    return 0;
+}
